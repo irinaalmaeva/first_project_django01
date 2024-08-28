8992ee92eaa80d7bf5f767c9f31a8c86de4b08a2
irinaalmaeva <almaeva.irina@gmail.com>
2024-08-28 23:44:20 +0300

    add data, test

--- flowers/basket/views.py
+++ flowers/basket/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Это большая корзина цветов для вас!</h1>")
+    return HttpResponse("<h1>Это первая большая корзина цветов для вас!</h1>")
+
+def data(request):
+    return HttpResponse("<h1>Это вторая большая корзина цветов для мамы!</h1>")
+
+
+def test(request):
+    return HttpResponse("<h1>Пройдите тест и определите какие цветы подходят вам больше всего!</h1>")
